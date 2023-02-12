947e65928b58279202fa3983ad635c390b4601ee
Ava Blake <avablakedesign@protonmail.com>
2023-02-12 15:10:21 -0500

    Updated schema and created scaffolding for resolvers.

---
+++ graphql/resolvers.ts
@@ -0,0 +1,40 @@
+export default {
+    Query: {
+        user: () => {
+            return {
+            }
+        },
+        users: () => {
+            return {
+            }
+        },
+        product: () => {
+            return {
+            }
+        },
+        products: () => {
+            return {
+            }
+        },
+        category: () => {
+            return {
+            }
+        },
+        categories: () => {
+            return {
+            }
+        },
+        cart: () => {
+            return {
+            }
+        },
+        order: () => {
+            return {
+            }
+        },
+        orders: () => {
+            return {
+            }
+        }
+    }
+}
--- graphql/schema.ts
+++ graphql/schema.ts
@@ -2,19 +2,47 @@ import { gql } from "graphql-tag"
 
 export default gql`
     type User{
-        id: String
-        name: String
-        age: Int
-        orders: [Product]
+        id: String!
+        first_name: String!
+        last_name: String!
+        address: String!
+        email: String!
+        password: String!
+        isAdmin: Boolean
+        orders: [Order]
+        cart: Cart
     }
     type Product{
-        id: String
-        name: String
-        description: String
-        price: Float
+        id: String!
+        name: String!
+        description: String!
+        price: Float!
+        quantity: Int
+        category: Category!
+    }
+    type Category{
+        id: String!
+        name: String!
+    }
+    type Cart{
+        id: String!
+        products: [Product]
+    }
+    type Order{
+        id: String!
+        #Apollo does not have date but we can use a string as the type.
+        purchaseDate: String
+        products: [Product!]
     }
     type Query{
-        user: User
-        product: Product
+        user(id:String, email:String): User
+        users: [User]
+        product(id:String!): Product
+        products: [Product]
+        category(id:String!): Category
+        categories: [Category]
+        cart(id:String!): Cart
+        order(id:String!): Order
+        orders: [Order]
     }
 `
