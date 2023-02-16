ecb6abb867f1eee852d4f9bd3f3cff60f3abb75d
Ava Blake <avablakedesign@protonmail.com>
2023-02-16 13:34:08 -0500

    Updated components functionality

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,17 @@
 import type { ReactNode } from "react"
+import Header from "./Header"
+import Footer from "./Footer"
 interface Props {
     children?:ReactNode
 }
 const Layout = (props:Props) => {
     return (
         <div>
+            <Header/>
             <main>
                 {props.children}
             </main>
+            <Footer/>
         </div>
     )
 }
